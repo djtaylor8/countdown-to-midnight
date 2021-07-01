252ff330657f541c3acf7324824adaac189331d0
DJ Taylor <dtaylor88@gmail.com>
2021-06-30 17:38:55 -0700

    commit tree 00536bbd82ca0f3f74c46840a647354083ba3ff4

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
+
 #write your code here
 
-def countdown
-end
+def countdown(integer)
+    x = integer
+    while x > 0
+      puts "#{x} SECOND(S)!"
+      x -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
+  
+  def countdown_with_sleep(integer)
+    sleep integer
+  end
